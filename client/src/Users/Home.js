8b4a374cd56695a0c0e18e596c793bdc05849870
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-15 11:49:39 +0100

    Home and Login pages created

---
+++ client/src/Users/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+    return (
+      <div className='home'>
+          <h1 className='intro'>Welcome to the Home page</h1>
+          <h3>This is the home of Jokes </h3>
+          <p>Please ensure you are registed and logged in to be able to view all jokes</p>
+      </div>
+    );
+  }
+  
+  export default Home;
