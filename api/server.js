53795aa4a6434a52b3be8c4f4c7ef2efef6c07d2
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-15 08:48:03 +0100

    Authentication middleware created

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res)=>{
+    res.json({message: "Welcome to the default zone, please specify a path"})
+})
+
 module.exports = server;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const secret = process.env.JWT_SECRET || "A secret lives here";
+  const token = req.headers.authorization;
+  if (token) {
+      jwt.verify(token, secret, (error, decodedToken) => {
+          if (error) {
+              res.status(401).json({ message: "Something went wrong:- " + error.message})
+          } else {
+              req.user = decodedToken.user
+              next()
+          }
+      })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
